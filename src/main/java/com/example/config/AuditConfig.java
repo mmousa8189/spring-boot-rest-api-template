64abfb2daa1220d15d34c98b6f8746d4f9edff1e
Mohamed Mousa <mohamedamousa00@gmail.com>
2025-08-30 21:54:05 +0300

    update currentAuditorProvider

--- src/main/java/com/example/config/AuditConfig.java
+++ src/main/java/com/example/config/AuditConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.nio.file.attribute.UserPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Optional;
 
 @Configuration
@@ -27,8 +27,11 @@ public class AuditConfig {
                 return Optional.of(SYSTEM_DEFAULT);
             }
             if (!authentication.getName().equals(ANONYMOUS_USER)) {
-                UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
-                return Optional.of(String.valueOf(""));
+                Object principal = authentication.getPrincipal();
+                if (principal instanceof UserDetails) {
+                    return Optional.of(((UserDetails) principal).getUsername());
+                }
+                return Optional.of(authentication.getName());
             } else {
                 if (authentication.getName().equals(ANONYMOUS_USER)) {
                     return Optional.of(ANONYMOUS_USER);
